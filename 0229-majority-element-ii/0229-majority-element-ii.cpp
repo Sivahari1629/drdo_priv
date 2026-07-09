9d8fc7f5b0a33f35e1015a4bafab4958c66a8271
Sivahari1629 <sivahari1629@gmail.com>
2026-07-09 16:43:39 +0530

    Time: 11 ms (19.56%), Space: 26.5 MB (15.59%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -11,6 +11,10 @@ public:
             {
                 ans.push_back(nums[i]);
             }
+            if(ans.size()==2)
+            {
+                break;
+            }
         }
         return ans;
         
