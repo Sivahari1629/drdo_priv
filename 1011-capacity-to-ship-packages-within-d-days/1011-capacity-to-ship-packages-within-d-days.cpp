e9535233876278301b289492816ab7bf399749df
Sivahari1629 <sivahari1629@gmail.com>
2026-08-10 19:16:44 +0530

    Time: 10 ms (69.88%), Space: 35 MB (54.97%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -22,13 +22,16 @@ return days;
     int shipWithinDays(vector<int>& weights, int days) {
         int low=*max_element(weights.begin(),weights.end());
         int high=accumulate(weights.begin(),weights.end(),0);
-        while(low<high)
+        int fin=0;
+        while(low<=high)
         {
             int mid=low+(high-low)/2;
             int d=cap(weights,mid);
+            
             if(d<=days)
             {
-                high=mid;
+                fin=mid;
+                high=mid-1;
             }
             else{
                 low=mid+1;
@@ -36,6 +39,6 @@ return days;
         
 
         }
-        return low;
+        return fin;
     }
 };
